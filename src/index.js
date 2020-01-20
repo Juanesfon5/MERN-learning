b6e6be20175d32f78dcd61506b5ccc0a662623d6
U-IPA\emc-jf <emc-jf@IPA-WN709.ipa.stuttgart>
2020-01-20 16:56:49 +0100

    First code

---
+++ src/index.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const app = express();
+
+//Settings
+
+//Middlewares
+
+//Routes
+
+//Static files
+
+//Start server
+app.listen(4000, ()=> {
+    console.log('server on port 4000');
+});
