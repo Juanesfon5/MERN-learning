3ea14474ca9a1f58bac1a5adfdf44491b3d26a03
U-IPA\emc-jf <emc-jf@IPA-WN709.ipa.stuttgart>
2020-01-21 16:42:31 +0100

    Finished app

--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const morgan = require('morgan');
+const path = require('path');
 const app = express();
+const {mongoose} = require('./database')
 
 //Settings
 app.set('port', process.env.PORT || 3000); 
@@ -8,11 +10,13 @@ app.set('port', process.env.PORT || 3000);
 
 //Middlewares
 app.use(morgan('dev'));
+app.use(express.json());
 
 //Routes
+app.use('/api/tasks' ,require	('./routes/task.routes'));
 
 //Static files
-
+app.use(express.static(path.join(__dirname, 'public')));
 //Start server
 app.listen(app.get('port'), ()=> {
     console.log('server on port '+app.get('port'));
